37c1423f155cc83bcb6dd87f10348aa342a43815
xaylax <119tintin@gmail.com>
2024-07-21 12:59:55 -0400

    test

--- MathGame.batus3010/MathGame.batus3010/Program.cs
+++ MathGame.batus3010/MathGame.batus3010/Program.cs
@@ -1,2 +1,29 @@
-﻿string testing = "test";
-Console.WriteLine($"this is a {testing}");
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity;
+string output;
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+const string openDiv = "<div>";
+const string closeDiv = "</div>";
+
+int openingPosition = input.IndexOf(openSpan);
+int closingPosition = input.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+quantity = input.Substring(openingPosition, length);
+
+///
+int openingPosition2 = input.IndexOf(openDiv);
+int closingPosition2 = input.IndexOf(closeDiv);
+
+openingPosition2 += openDiv.Length;
+int length2 = closingPosition2 - openingPosition2;
+
+output = input.Substring(openingPosition2, length2);
+output = output.Replace("trade", "reg");
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
